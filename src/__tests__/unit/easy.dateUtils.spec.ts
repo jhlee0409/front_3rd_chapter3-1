997f395f7e3abfe327f7b897566e08c3e1bdc4a3
Jack <relee6203@gmail.com>
2024-11-03 00:50:38 +0900

    test: dateUtils - getDaysInMonth

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -12,15 +12,30 @@ import {
 } from '../../utils/dateUtils';
 
 describe('getDaysInMonth', () => {
-  it('1월은 31일 수를 반환한다', () => {});
+  it('1월은 31일 수를 반환한다', () => {
+    const days = getDaysInMonth(2024, 1);
+    expect(days).toBe(31);
+  });
 
-  it('4월은 30일 일수를 반환한다', () => {});
+  it('4월은 30일 일수를 반환한다', () => {
+    const days = getDaysInMonth(2024, 4);
+    expect(days).toBe(30);
+  });
 
-  it('윤년의 2월에 대해 29일을 반환한다', () => {});
+  it('윤년의 2월에 대해 29일을 반환한다', () => {
+    const days = getDaysInMonth(2024, 2);
+    expect(days).toBe(29);
+  });
 
-  it('평년의 2월에 대해 28일을 반환한다', () => {});
+  it('평년의 2월에 대해 28일을 반환한다', () => {
+    const days = getDaysInMonth(2023, 2);
+    expect(days).toBe(28);
+  });
 
-  it('유효하지 않은 월에 대해 적절히 처리한다', () => {});
+  it('유효하지 않은 월에 대해 -1을 반환한다', () => {
+    const days = getDaysInMonth(2024, 14);
+    expect(days).toBe(-1);
+  });
 });
 
 describe('getWeekDates', () => {
